ebd36626b7e570e64f3a73971858f85c2b3388c9
SWETHA RAJULU <55911880+srajulu@users.noreply.github.com>
2020-10-15 19:41:33 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,8 +6,9 @@ public class BubbleSort{
     for(var i=0; i<array.length; i++)
       for(var j=i+1;j<array.length;j++)
         if(array[j]<array[i]){
-          var temp = 
-          
-
+          var temp = array[j];
+          var array[j] = array[i];
+          var array[i] = temp;
+        }
  }
 }
