2da2bd6d3d3c483b27039ddefe032cfa9fb9c16f
SWETHA RAJULU <55911880+srajulu@users.noreply.github.com>
2020-10-17 21:28:26 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -16,9 +16,9 @@ public class BubbleSort{
 
  private void swap(int[] array, int index1,int index2){
 
-
-
-
+    var temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
 
 
   }
